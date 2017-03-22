f554ed9f9bf66fcde4654f9d3a8ff91268d0a6c3
asauze <asauze@ASAUZE17-DE>
2017-03-22 15:12:40 +0100

    Nouveau Algo > Trier un tableau

--- Exercices/Nombres premier Correction/Program.cs
+++ Exercices/Nombres premier Correction/Program.cs
@@ -8,31 +8,49 @@ namespace NombresPremier
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main(string[] args) // Main = point d'entrée de tout programme
 		{
-			int divis, nbr, n, compt = 0;
-			bool Est_premier;
-			Console.Write("Combien de nombres premiers : ");
-			n = Int32.Parse(System.Console.ReadLine());
-			Console.WriteLine(2);
-			nbr = 3;
-			while (compt < n - 1)
-			{
-				divis = 2;
-				Est_premier = true;
-				do
-				{
-					if (nbr % divis == 0) Est_premier = false;
-					else divis = divis + 1;
-				}
-				while ((divis <= nbr / 2) && (Est_premier == true));
-				if (Est_premier)
-				{
-					compt++;
-					System.Console.WriteLine(nbr);
-				}
-				nbr++;
-			}
-		}
+            int n;
+			
+			Console.Write("Combien de nombres premiers : "); // Write écrit sur la même ligne, pas de nouvelle ligne. permet d'afficher un message
+            // string réponse; // on déclare une variable string
+            // réponse = console.readline; // La réponse correspondra a ce qu'on a saisi au clavier avant
+            // n = int.Pars(réponse); On dit que la variable n défini plus au correspond à la traduction de réponse en un entier
+			n = Int32.Parse(System.Console.ReadLine()); // équivalent à au dessus en plus condensé.
+            claculerNombrePremier(n);
+            claculerNombrePremier(15);
+            claculerNombrePremier(19);
+            claculerNombrePremier(n);
+
+            Console.ReadKey();
+
+        }
+
+        static void claculerNombrePremier(int nbPremier )
+        {
+            int divis, nbr, cptPremier = 0; // On déclare des variables qu'on a besoin après. On initilise à 0 car après on cherchera tout de suite à le comparer
+            bool estPremier; // Variables booléene c'est à dire qui peut etre vrai ou fausse (true / false).
+
+            Console.WriteLine(2); // On dit que 2 est un nombre 1er, la console l'affichera d'office
+            nbr = 3; // donc je commence à 3 // au aura pu initialise nbr plus haut direct à 3
+            while (cptPremier < nbPremier - 1) // Boucle While, on a déja écrit 2 donc on demande au compteur de faire -1
+            {
+                divis = 2;
+                estPremier = true; // au début on suppose que le nombre est premier
+                do
+                {
+                    if (nbr % divis == 0) estPremier = false; // On regarde si le reste est de 0, c'est c'es la cas, nombre premier = faux
+                    else divis = divis + 1; // incrémenter si le nombre est premier
+                }
+                while ((divis <= nbr / 2) && (estPremier == true)); // je fait la boucle tant que le diviseur ....
+                if (estPremier)
+                {
+                    cptPremier++;
+                    System.Console.WriteLine(nbr);
+                }
+                nbr++;
+            }
+
+        }
 	}
 }
---
+++ Exercices/trier un tableau/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trier_un_tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Création et insertion des infos dans le tableau + création de stock
+            string[] tabPos = new string[4]; //on peut aussi initialiser les éléments de cette façon : {"Alfa", "Charli", ...);
+            tabPos[0] = "Alfa ";
+            tabPos[1] = "Charlie ";
+            tabPos[2] = "Sierra ";
+            tabPos[3] = "Bravo ";
+            string stock;
+
+            //Affichage du tableau
+            //Console.WriteLine(tabPos[0] + tabPos[1] + tabPos[2] + tabPos[3]);
+            FonctionAffichage(tabPos);
+            //Reglage de la boucle
+            TrierTableau(tabPos);
+            //for (int compteurlongueurtab = 0; compteurlongueurtab < tabPos.Length - 1; compteurlongueurtab++)
+            {
+                //Comparaison
+               // int resultatComparaison = tabPos[compteurlongueurtab].CompareTo(tabPos[compteurlongueurtab + 1]);
+
+                //Condition
+               // if (resultatComparaison > 0)
+                {
+                  //  stock = tabPos[compteurlongueurtab + 1];
+                   // tabPos[compteurlongueurtab + 1] = tabPos[compteurlongueurtab];
+                   // tabPos[compteurlongueurtab] = stock;
+                   // compteurlongueurtab = -1;
+                }
+
+            }
+
+
+            Console.WriteLine(tabPos[0] + tabPos[1] + tabPos[2] + tabPos[3]);
+
+            Console.ReadKey();
+
+        }
+
+        static void FonctionAffichage(string[] tableau)
+        {
+            for (int compteurlongueurtab = 0; compteurlongueurtab < tableau.Length; compteurlongueurtab++)
+                Console.Write(" " + tableau[compteurlongueurtab]);
+                Console.WriteLine(" ");
+        }
+
+        static void TrierTableau(string[] tableau)
+        {
+            for (int compteurlongueurtab = 0; compteurlongueurtab < tableau.Length - 1; compteurlongueurtab++)
+            {
+                int resultatComparaison = tableau[compteurlongueurtab].CompareTo(tableau[compteurlongueurtab + 1]);
+                if (resultatComparaison > 0)
+                {
+                    string stock;
+                    stock = tableau[compteurlongueurtab + 1];
+                    tableau[compteurlongueurtab + 1] = tableau[compteurlongueurtab];
+                    tableau[compteurlongueurtab] = stock;
+                    compteurlongueurtab = -1;
+                }
+           }
+        }
+    }
+
+}
