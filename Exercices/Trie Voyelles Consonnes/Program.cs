afaf82b1f3cf0259a58775d888d821cddef183df
asauze <asauze@ASAUZE17-DE>
2017-03-23 14:48:42 +0100

    Misea à jour avec exercice Consonnes, Voyelles

---
+++ Exercices/Trie Voyelles Consonnes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trie_Voyelles_Consonnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Saisie du mot + création de la variable mot pour le stocker
+            Console.WriteLine(@"Saisissez un mot SVP");
+            string mot = Console.ReadLine();
+            int nbConsonnes, nbVoyelles;
+            CompterConsonnes(mot, out nbConsonnes);
+            Console.WriteLine("Le mot comporte {0} consonnes, {1} de voyelles.", nbConsonnes, nbVoyelles);
+
+        }
+        static void CompterConsonnes(string mot, out int consonnes)
+        {
+            for(int compteurMots = 0; compteurMots < mot.Length; compteurMots++)
+            {
+                if (mot[compteurMots] != 'a'  )
+
+            }
+
+
+
+
+        }
+
+    }
+}
+
---
+++ Exercices/compter Voyelles consonnes correction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compter_Voyelles_consonnes_correction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("saisissez un mot");
+            string mot = Console.ReadLine();
+
+            int nbVoyelles, nbConsonnes;
+            CompterVoyellesConsonnes(mot, out nbVoyelles, out nbConsonnes);
+            Console.WriteLine("Le mot {0} comporte {1} voyelles et {2} consonnes", mot, nbVoyelles, nbConsonnes);
+            Console.ReadKey();
+        }
+
+        static void CompterVoyellesConsonnes(string mot, out int nbVoy, out int nbCons)
+        {
+            nbVoy = 0;
+
+            for (int i = 0; i < mot.Length; i++)
+            {
+                if (mot[i] == 'a' || mot[i] == 'e' || mot[i] == 'i' || mot[i] == 'o' || mot[i] == 'u' || mot[i] == 'y')
+                {
+                    nbVoy++;
+                }
+            }
+            nbCons = mot.Length - nbVoy;
+
+        }
+    }
+}
