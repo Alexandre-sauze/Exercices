5046142a2b83f996c8e61b5aaf7e56aba3dea356
asauze <asauze@ASAUZE17-DE>
2017-03-29 08:39:03 +0200

    MAJ

---
+++ Exercices/Véhicules/Moto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Véhicules
+{
+    class Moto:Véhicule
+    {
+        public Moto(string nom, Energies energie) : base(nom, 2, energie)
+        {
+
+        }
+        public override string Description
+        {
+            get
+            {
+                return "Je suis une moto\r\n" + base.Description;
+            }
+        }
+    }
+}
--- Exercices/Véhicules/Program.cs
+++ Exercices/Véhicules/Program.cs
@@ -10,7 +10,11 @@ namespace Véhicules
     {
         static void Main(string[] args)
         {
-            Véhicule V1 = new Voiture("Porsche", Energies.Essence);
+            Véhicule V1 = new Voiture("Porsche 911", Energies.Essence);
+            Console.WriteLine(V1.Description);
+            Véhicule v2 = new Moto("kawasaki NinjaR", Energies.Essence);
+            Console.WriteLine(v2.Description);
+
         }
     }
 
